c92a043ecaf048309ff6143c2fc9e121a2aa3fdc
sufibaig1 <sufibaig610@gmail.com>
2025-09-01 13:46:47 +0500

    Add files via upload

---
+++ tospliced.js
@@ -0,0 +1,4 @@
+const arr=[1,2,3,4,5,6,7,8,9];
+console.log( arr.toSpliced(6,8,56,89,90,45));
+// toSPliced(startindex,lastindex,items...)
+// It doesn't mutate the original array.It returns a new array with the specified items spliced in.
